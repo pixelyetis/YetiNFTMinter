2ca71a11ce0561d10c003b669816b528abbb82e2
El Kebabo King <elkebaboking@gmail.com>
2022-02-10 14:47:10 +0100

    YetiMinter can now mint a new yeti through a public function.

--- contracts/YetiMinter.sol
+++ contracts/YetiMinter.sol
@@ -4,45 +4,53 @@ pragma solidity >= 0.5.0;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+// import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract YetiMinter {
+contract YetiMinter is ERC721Enumerable{
 	using SafeMath for uint256;
+
 	
-	// struct Yeti {
-	// 	string accessory;
-	// 	string eyes;
-	// 	string fur;
-	// 	string horns;
-	// 	string mouth;
-	// 	string skin;
-	// }
+	struct Yeti {
+		string accessory;
+		string eyes;
+		string fur;
+		string horns;
+		string mouth;
+		string skin;
+	}
 
-	Yeti[] public yetis;
+	Yeti[] private yetis;
 
-	string[] private eyes = [
-		"Angry Eyes",
-		"Bored Eyes",
-		"Eye Patch",
-		"Glasses",
-		"Happy Eyes",
-		"Normal Eyes",
-		"Pissed Eyes",
-		"Shades",
-		"Sleeping Eyes"];
+	string[] private accessories = ["Beanie", "Bowtie", "Cigarette", "Fast Food","Headphones", "None", "Nose Piercing"];
+	string[] private eyes = ["Angry Eyes","Bored Eyes", "Eye Patch", "Glasses", "Happy Eyes", "Normal Eyes", "Pissed Eyes", "Shades", "Sleeping Eyes"];
+	string[] private fur = ["Beige Fur", "Blue Fur", "Blue Fur Striped", "Gray Fur", "Gray Fur Checkered", "Gray Fur", "Pixelated Fur", "Rainbow Fur", "White Fur", "White Fur Striped"];
+	string[] private horns = ["Black Horns", "Blue Horns", "Orange Horns", "Red Horns", "Yellow Horns"];
+	string[] private mouth = ["Happy Mouth", "Normal Mouth", "Rainbow Mouth", "Rainbow Mouth Wave 1", "Rainbow Mouth Wave 2", "Sad Mouth", "Teeth"];
+	string[] private skin = ["Blue Skin", "Dark Blue Skin", "Green Skin",  "Orange Skin", "Pink Skin", "Purple Skin"];
+
+
+	constructor() ERC721('TestNFT', 'TN') {
+
+	}
+
 
 	// Function to mint A new Yeti NFT.
-	function mintYeti() public {
+	function mintYeti() public{
+
+		uint256 mintIndex = totalSupply();
+
+		yetis.push(Yeti(
+			accessories[_generateRandomAttribute(mintIndex, "ACCESSORIES", accessories)],
+			eyes[_generateRandomAttribute(mintIndex, "EYES", eyes)], 
+			fur[_generateRandomAttribute(mintIndex, "FUR", fur)],
+			horns[_generateRandomAttribute(mintIndex, "HORNS", horns)], 
+			mouth[_generateRandomAttribute(mintIndex, "MOUTH", mouth)], 
+			skin[_generateRandomAttribute(mintIndex, "SKIN", skin)]
+			));
+
+		_safeMint(msg.sender, mintIndex);
 
 
-		// yetis.push(Yeti(
-		// 	accessory, 
-		// 	eyes, 
-		// 	fur, 
-		// 	horns, 
-		// 	mouth, 
-		// 	skin
-		// 	));
 	}
 
 	function _generateRandomAttribute(uint256 _yetiId, string memory _attribute, string[] memory _attributeList) private view returns(uint256) {
