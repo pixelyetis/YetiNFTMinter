b67378d45f2d79243369fe8a7e399cb23b375d66
El Kebabo King <elkebaboking@gmail.com>
2022-02-14 15:13:27 +0100

    Add Blizz contract change where 100k is minted instead of 1k at deployment.

--- contracts/Blizz.sol
+++ contracts/Blizz.sol
@@ -4,12 +4,17 @@ pragma solidity >=0.5.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Blizz is ERC20{
+
 	constructor(string memory name, string memory symbol) ERC20(name, symbol){
 		// Mint 1000 token to msg.sender
-		_mint(msg.sender, 1000 * 10 ** uint(decimals()));
+		_mint(msg.sender, 100000 * 10 ** uint(decimals()));
 	}
 
 	function getAddr() external view returns(address){
 		return address(this);
+	}
+
+	function gimmieSome(uint _amount) external {
+		_mint(msg.sender, _amount * 10 ** uint(decimals()));
 	}
 }
