1539936fd45a5d23261e22dd6f08207dba19a4a3
El Kebabo King <elkebaboking@gmail.com>
2022-02-15 11:44:37 +0100

    - Comment out console logs - Add new test for ensuring owner correctly receives all funds.

--- test/YetiMinter.js
+++ test/YetiMinter.js
@@ -37,33 +37,35 @@ contract('YetiMinter', (accounts) =>{
 		await blizz.approve(await yeti.getAddr(), '0xffffffffffffffffffffff')
 
 		// Mint
-		console.log('Before mint: ' + await yeti.totalSupply());
+		// console.log('Before mint: ' + await yeti.totalSupply());
+		const beforeMint = await yeti.totalSupply()
 		await yeti.mintYeti(amount);
-		console.log('After mint: ' + await yeti.totalSupply());
+		const afterMint = await yeti.totalSupply()
+		assert(afterMint > beforeMint)
+		// console.log('After mint: ' + await yeti.totalSupply());
 	})
 
 	it('Should fail to mint more NFTs than maximum supply', async()=>{
-		const amount = '0x2B5E3AF16B1880000';
+		const amount = '0x2B5E3AF16B1880000'; // 100 ether units in hex
 		await yeti.setMaxSuply(10)
+
 		// Mint till supply cap
 		for(let i = await yeti.totalSupply(); i < await yeti.getMaxSupply(); i++){
 			await yeti.mintYeti(amount);
 		}
-		console.log('MAX SUPPLY: ' + await yeti.getMaxSupply())
-		console.log('Current supply: ' + await yeti.totalSupply())
+		// console.log('MAX SUPPLY: ' + await yeti.getMaxSupply())
+		// console.log('Current supply: ' + await yeti.totalSupply())
 
 		try{
 			await yeti.mintYeti(amount)
 		}catch(err){
 			assert(err.message.search('Max supply reached.') > 0)
 		}
-		console.log('Current supply: ' + await yeti.totalSupply())
+		// console.log('Current supply: ' + await yeti.totalSupply())
 	})
 
 	// Test only owner can make crucial changes.
 	it('Should ensure only owner can make changes to contract', async()=>{
-		console.log(accounts[1])
-
 		// From deployer
 		await yeti.setMintTokenAddress(blzAddr, {from: accounts[0]})
 
@@ -75,6 +77,23 @@ contract('YetiMinter', (accounts) =>{
 		}catch(err){
 			assert(err.message.search('Ownable: caller is not the owner') > 0)
 		}
+	})
+
+	it('Should withdraw balance of contract to owner', async()=>{
+		const contractBefore = await blizz.balanceOf(await yeti.getAddr())
+		const ownerBefore = await blizz.balanceOf(sender)
+		// console.log('Balance of contract before withdraw: ' + await blizz.balanceOf(await yeti.getAddr()))
+		// console.log('Balance of owner before withdraw: ' + await blizz.balanceOf(sender))
+		await yeti.withdraw()
+
+		const contractAfter = await blizz.balanceOf(await yeti.getAddr())
+		const ownerAfter = await blizz.balanceOf(sender)
+
+		assert(ownerAfter - ownerBefore > 0)
+		assert(contractBefore - contractAfter > 0)
+
+		// console.log('Balance of contract after withdraw: ' + await blizz.balanceOf(await yeti.getAddr()))
+		// console.log('Balance of owner after withdraw: ' + await blizz.balanceOf(sender))
 	})
 
 	it('Should get contract ABIs', async()=> {			
